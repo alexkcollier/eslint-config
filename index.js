ce80c2c7ff2f63b43dab4d630ec8cf8a6e909487
Alex Collier <alexkcollier@gmail.com>
2019-06-25 21:20:55 -0400

    feat: removes prettier, migrates to yarn

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  extends: ['standard', 'plugin:prettier/recommended', 'prettier'],
-  plugins: ['standard', 'prettier'],
+  extends: ['standard'],
+  plugins: ['standard'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.mjs'],
+      },
+    },
+  },
   rules: {
-    semi: [2, 'never'],
-    'no-console': 'off',
-    'no-debugger': 'off',
-    'prettier/prettier': [
+    'array-bracket-newline': ['error', 'consistent'],
+    'array-element-newline': ['error', 'consistent'],
+    'arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
+    camelcase: ['warn', { ignoreDestructuring: true }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'max-len': [
       'error',
       {
-        semi: false,
-        singleQuote: true,
-        printWidth: 100
-      }
-    ]
-  }
+        code: 100,
+        ignoreStrings: true,
+        ignoreUrls: true,
+        ignoreComments: true,
+      },
+    ],
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 0,
+    'no-return-assign': ['error', 'always'],
+    'object-curly-newline': [
+      'error',
+      {
+        consistent: true,
+        multiline: true,
+      },
+    ],
+  },
 }
